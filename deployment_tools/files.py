0b679a9ec2e88d1283da3e63fc4a3e6a4e213377
Daniel Nikolaev <toolsproed@gmail.com>
2022-11-29 20:45:18 +0200

    updates to file

--- deployment_tools/files.py
+++ deployment_tools/files.py
@@ -1,48 +1,56 @@
 from __future__ import annotations
 from dataclasses import dataclass
-import os
 
 
 @dataclass
-class Replace:
-    old_str: str
-    new_str: str
+class _Replace:
+    new_str: str = ''
+    old_str: str = ''
+    new_line: set = None
     pattern: str = None
 
-    def replace_in_line(self, line: str) -> str:
-        if self.pattern is not None and self.pattern not in line:
-            return line
+    def make_updates(self, line: str) -> str:
+        if self.pattern is not None:
+            if self.pattern not in line:
+                return line
+            if self.new_line is not None:
+                print(f"\t{line} => {self.new_line}")
+                return self.new_line
         print(f"\t{self.old_str} => {self.new_str}")
         return line.replace(self.old_str, self.new_str)
 
 
 @dataclass
-class NewLine:
+class _NewLine:
     line: str
     position: int or None = None
+
+    def self_insert_into(self, lines_list: list[str]) -> list[str]:
+        if self.position is None:
+            lines_list.append(self.line)
+        else:
+            lines_list.insert(self.position, self.line)
+        return lines_list
 
 
 class FileTransformer:
     def __init__(self, path: str) -> None:
         self.path: str = path
-        self._updates: dict[str:str] = dict()
-        self._replaces: list[Replace] = []
-        self._add_lines: list[NewLine] = []
+        self._replaces: list[_Replace] = []
+        self._lines_to_add: list[_NewLine] = []
 
-    def replace_line(self, if_line_contains: str, new_line: str) -> FileTransformer:
-        self._updates[if_line_contains] = self._ensure_newline(new_line)
+    def replace_line(self, pattern: str, new_line: str) -> FileTransformer:
+        self._replaces.append(_Replace(new_line=new_line, pattern=pattern))
+        return self
+
+    def replace_in_line(self, *, pattern: str = None, old_str: str, new_str: str) -> FileTransformer:
+        self._replaces.append(_Replace(old_str=old_str, new_str=new_str, pattern=pattern))
         return self
 
     def add_line(self, line: str or list[str], row=None):
         lines = [line] if isinstance(line, str) else list(line)[::-1]
         for line in lines:
-            self._add_lines.append(NewLine(self._ensure_newline(line), row))
-        return self
-
-    def replace_in_line(self, *, pattern: str = None, old_str: str, new_str: str) -> FileTransformer:
-        self._replaces.append(
-            Replace(old_str=old_str, new_str=new_str, pattern=pattern)
-        )
+            self._lines_to_add.append(_NewLine(self._ensure_newline(line), row))
         return self
 
     @staticmethod
@@ -52,13 +60,8 @@ class FileTransformer:
         return line
 
     def _update_line(self, line: str) -> str:
-        for pattern, new_line in self._updates.items():
-            if pattern in line:
-                print(f"{self.path} => setting {new_line}")
-                return new_line
-        for replace in self._replaces:
-            print(f"{self.path} => replacements:")
-            return replace.replace_in_line(line)
+        for line_replace in self._replaces:
+            line = line_replace.make_updates(line)
         return line
 
     def _get_current_file_lines(self) -> list[str]:
@@ -70,10 +73,7 @@ class FileTransformer:
 
     def apply(self):
         new_lines = [self._update_line(line) for line in self._get_current_file_lines()]
-        for line_to_add in self._add_lines:
-            if line_to_add.position is None:
-                new_lines.append(line_to_add.line)
-            else:
-                new_lines.insert(line_to_add.position, line_to_add.line)
+        for line_to_add in self._lines_to_add:
+            line_to_add.self_insert_into(new_lines)
         with open(self.path, 'w') as new_file:
             new_file.writelines(new_lines)
